3db5601651743c40e5f3ae3708915459f418ccc0
Jim Haff <Jimmyhaff3@gmail.com>
2022-09-23 14:37:20 -0500

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,45 +1,67 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-const SECRET = process.env.SECRET;
-const { v4: uuidv4 } = require('uuid');
-const S3 = require('aws-sdk/clients/s3');
-const s3 = new S3(); // initialize the construcotr
-// now s3 can crud on our s3 buckets
+const S3 = require("aws-sdk/clients/s3");
+const s3 = new S3(); // initate the S3 constructor which can talk to aws/s3 our bucket!
+// import uuid to help generate random names
+const { v4: uuidv4 } = require("uuid");
+// since we are sharing code, when you pull you don't want to have to edit the
+// the bucket name, thats why we're using an environment variable
+const BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+
 
 module.exports = {
   signup,
   login
 };
 
-function signup(req, res) {
-  console.log(req.body, req.file)
+async function signup(req, res) {
+  console.log(req.body, " req.body in signup", req.file);
 
-  //////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////
+  if (!req.file) return res.status(400).json({ error: "Please submit Photo!" });
+  // Create the key that we will store in the s3 bucket name
+  // pupstagram/ <- will upload everything to the bucket so it appears
+  // like its an a folder (really its just nested keys on the bucket)
+  const key = `pupstagram/${uuidv4()}-${req.file.originalname}`;
+  const params = { Bucket: BUCKET_NAME, Key: key, Body: req.file.buffer };
 
-  // FilePath unique name to be saved to our butckt
-  const filePath = `${uuidv4()}/${req.file.originalname}`
-  const params = {Bucket: process.env.BUCKET_NAME, Key: filePath, Body: req.file.buffer};
-  //your bucket name goes where collectorcat is 
-  //////////////////////////////////////////////////////////////////////////////////
-  s3.upload(params, async function(err, data){
-    console.log(data, 'from aws') // data.Location is our photoUrl that exists on aws
-    const user = new User({...req.body, photoUrl: data.Location});
+  s3.upload(params, async function (err, data) {
+    // this function is called when we get a response from AWS
+    // inside of the callback is a response from AWS!
+    console.log("========================");
+    console.log(err, " <--- err from aws");
+    console.log("========================");
+    if (err)
+      return res.status(400).json({
+        err: "Error from aws, check the server terminal!, you bucket name or keys are probley wrong",
+      });
+
+    // data.Location <- should be the say as the key but with the aws domain
+    // its where our photo is hosted on our s3 bucket
+    const user = new User({ ...req.body, photoUrl: data.Location });
     try {
       await user.save();
-      const token = createJWT(user); // user is the payload so this is the object in our jwt
-      res.json({ token });
+      const token = createJWT(user);
+      res.json({ token }); // shorthand for the below
+      // res.json({ token: token })
     } catch (err) {
-      // Probably a duplicate email
-      res.status(400).json(err);
+      if (err.name === "MongoServerError" && err.code === 11000) {
+        console.log(err.message, "err.message");
+        res
+          .status(423)
+          .json({
+            errorMessage: err,
+            error: `${identifyKeyInMongooseValidationError(
+              err.message
+            )} Already taken!`,
+          });
+      } else {
+        res.status(500).json({
+          error: err,
+          message: "Internal Server Error, Please try again",
+        });
+      }
     }
-
-
-
-  })
-  //////////////////////////////////////////////////////////////////////////////////
- 
+  });
 }
 
 async function login(req, res) {
@@ -71,4 +93,10 @@ function createJWT(user) {
     SECRET,
     {expiresIn: '24h'}
   );
+}
+
+function identifyKeyInMongooseValidationError(err) {
+  let key = err.split("dup key: {")[1].trim();
+  key = key.slice(0, key.indexOf(":"));
+  return key.replace(/^./, (str) => str.toUpperCase());
 }
